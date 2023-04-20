0e2d0524e690e8bbe0e27867b6e9eb531508543c
BuildTools <unconfigured@null.spigotmc.org>
2023-04-19 22:30:43 -0700

    Added our pre-made providers

---
+++ src/main/java/com/moleculepowered/api/updater/provider/BukkitProvider.java
@@ -0,0 +1,148 @@
+package com.moleculepowered.api.updater.provider;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.moleculepowered.api.util.Version;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Set;
+
+public class BukkitProvider extends AbstractProvider {
+
+    private final String API_KEY;
+    private final int resourceID;
+    private String downloadLink, latestVersion;
+
+    public BukkitProvider(int resourceID, @Nullable String apiKey) {
+        this.resourceID = resourceID;
+        this.API_KEY = apiKey;
+    }
+
+    public BukkitProvider(int resourceID) {
+        this(resourceID, null);
+    }
+
+    /**
+     * A method used to fetch the latest information for this provider, it handles
+     * the main logic and is recommended to only assign values to the other methods
+     * within this provider, such as changelog link, download link, etc.
+     */
+    @Override
+    public void fetch() {
+        try {
+            HttpURLConnection conn = connection("https://api.curseforge.com/servermods/files?projectIds={0}", resourceID);
+
+            // ADD API KEY IF PRESENT
+            if (API_KEY != null && !API_KEY.equals("NONE")) conn.addRequestProperty("X-API-Key", API_KEY);
+            conn.connect();
+
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            JsonArray array = new Gson().fromJson(reader, JsonArray.class);
+            JsonObject resource = array.get(array.size() - 1).getAsJsonObject();
+
+            latestVersion = resource.get("name").getAsString();
+            downloadLink = resource.get("downloadUrl").getAsString();
+
+            // CLOSE CONNECTION
+            conn.disconnect();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the name assigned to this provider, this name will be used for logging messages
+     *
+     * @return Provider name
+     */
+    @Override
+    public @NotNull String getName() {
+        return "Bukkit";
+    }
+
+    /**
+     * Returns the url pointing to the location the latest update can be downloaded from.
+     *
+     * @return Download link
+     */
+    @Override
+    public @Nullable String getDownloadLink() {
+        return downloadLink;
+    }
+
+    /**
+     * Returns the url pointing to the changelog for the latest update
+     *
+     * @return Update changelog
+     */
+    @Override
+    public @Nullable String getChangelogLink() {
+        return null;
+    }
+
+    /**
+     * Return's the author's donation link
+     *
+     * @return Donation link
+     */
+    @Override
+    public String getDonationLink() {
+        return null;
+    }
+
+    /**
+     * Returns the latest version number returned by this provider
+     *
+     * @return Latest version number
+     */
+    @Override
+    public @NotNull Version getLatestVersion() {
+        return new Version(latestVersion);
+    }
+
+    /**
+     * Return's the price of the latest update
+     *
+     * @return Update price
+     */
+    @Override
+    public @Nullable String getPrice() {
+        return null;
+    }
+
+    /**
+     * Returns a set of contributors for the latest update
+     *
+     * @return Set of contributors
+     */
+    @Override
+    public @Nullable Set<String> getContributors() {
+        return null;
+    }
+
+    /**
+     * Used to return the total number of downloads for this release
+     *
+     * @return Total downloads
+     */
+    @Override
+    public int getDownloads() {
+        return 0;
+    }
+
+    /**
+     * Return's true if the latest release is a premium resource
+     *
+     * @return true if premium
+     */
+    @Override
+    public boolean isPremium() {
+        return false;
+    }
+}
---
+++ src/main/java/com/moleculepowered/api/updater/provider/PolymartProvider.java
@@ -0,0 +1,161 @@
+package com.moleculepowered.api.updater.provider;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.moleculepowered.api.util.Version;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PolymartProvider extends AbstractProvider {
+
+    private final Set<String> contributors = new HashSet<>();
+    private final int resourceID;
+    private String downloadLink, latestVersion, changelogLink;
+    private String price;
+    private int totalDownloads;
+
+    public PolymartProvider(int resourceID) {
+        this.resourceID = resourceID;
+    }
+
+    public PolymartProvider(String resourceID) {
+        this(Integer.parseInt(resourceID));
+    }
+
+    /**
+     * A method used to fetch the latest information for this provider, it handles
+     * the main logic and is recommended to only assign values to the other methods
+     * within this provider, such as changelog link, download link, etc.
+     */
+    @Override
+    public void fetch() {
+        try {
+            HttpURLConnection conn = connection("https://api.polymart.org/v1/getResourceInfo/?pretty_print_result=1&resource_id={0}", resourceID);
+
+            // CREATE A READER FOR JSON PARSING
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            // CREATE JSON OBJECTS FOR REQUESTED VALUES
+            JsonObject response = new Gson().fromJson(reader, JsonObject.class).getAsJsonObject("response");
+            JsonObject resource = response.getAsJsonObject("resource");
+            JsonObject owner = resource.getAsJsonObject("owner");
+            JsonObject latest = resource.getAsJsonObject("updates").getAsJsonObject("latest");
+
+            // SET INFORMATION VALUES
+            latestVersion = latest.get("version").getAsString();
+            price = resource.get("price").getAsString() + " " + resource.get("currency").getAsString();
+            totalDownloads = resource.get("downloads").getAsInt();
+
+            // SETTING LINK VALUES
+            String rawDownloadLink = resource.get("url").getAsString();
+            int hangingIndex = rawDownloadLink.indexOf("?");
+            downloadLink = rawDownloadLink.substring(0, hangingIndex);
+            changelogLink = downloadLink + "/updates";
+
+            // ADD CONTRIBUTORS
+            contributors.add(owner.get("name").getAsString());
+
+            // DISCONNECT
+            conn.disconnect();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the name assigned to this provider, this name will be used for logging messages
+     *
+     * @return Provider name
+     */
+    @Override
+    public @NotNull String getName() {
+        return "Polymart";
+    }
+
+    /**
+     * Returns the url pointing to the location the latest update can be downloaded from.
+     *
+     * @return Download link
+     */
+    @Override
+    public @Nullable String getDownloadLink() {
+        return downloadLink;
+    }
+
+    /**
+     * Returns the url pointing to the changelog for the latest update
+     *
+     * @return Update changelog
+     */
+    @Override
+    public @Nullable String getChangelogLink() {
+        return changelogLink;
+    }
+
+    /**
+     * Returns the latest version number returned by this provider
+     *
+     * @return Latest version number
+     */
+    @Override
+    public @NotNull Version getLatestVersion() {
+        return new Version(latestVersion);
+    }
+
+    /**
+     * Return's the author's donation link
+     *
+     * @return Donation link
+     */
+    @Override
+    public String getDonationLink() {
+        return null;
+    }
+
+    /**
+     * Return's the price of the latest update
+     *
+     * @return Update price
+     */
+    @Override
+    public @Nullable String getPrice() {
+        return price;
+    }
+
+    /**
+     * Returns a set of contributors for the latest update
+     *
+     * @return Set of contributors
+     */
+    @Override
+    public @Nullable Set<String> getContributors() {
+        return contributors;
+    }
+
+    /**
+     * Used to return the total number of downloads for this release
+     *
+     * @return Total downloads
+     */
+    @Override
+    public int getDownloads() {
+        return totalDownloads;
+    }
+
+    /**
+     * Return's true if the latest release is a premium resource
+     *
+     * @return true if premium
+     */
+    @Override
+    public boolean isPremium() {
+        return price != null && price.contains("0.00");
+    }
+}
