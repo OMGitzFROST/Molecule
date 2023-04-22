4c177a7fe9497d8f5b64af5f9fc98f4995a22b45
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 09:20:45 -0700

    Added more formatting methods

--- src/main/java/com/moleculepowered/api/util/StringUtil.java
+++ src/main/java/com/moleculepowered/api/util/StringUtil.java
@@ -1,12 +1,17 @@
 package com.moleculepowered.api.util;
 
 import com.google.gson.JsonElement;
+import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.text.MessageFormat;
 
 public final class StringUtil {
+
+    /*
+    FORMATTING METHODS
+     */
 
     /**
      * Formats a string to include the optional parameters within its formatted self. This method
@@ -18,8 +23,48 @@ public final class StringUtil {
      * @return A formatted string
      */
     public static @NotNull String format(String input, Object... param) {
-        return MessageFormat.format(input, param);
+        return translateColor(MessageFormat.format(input, param));
     }
+
+    /**
+     * Used to return a formatted string with color codes removed. This method
+     * utilizes number formats such as "{0}" within the string in order to replace it with its
+     * assigned value.
+     *
+     * @param strip Whether color codes should be stripped
+     * @param input Provided input
+     * @param param Optional Params
+     * @return A formatted, colorless string
+     */
+    public static @NotNull String format(boolean strip, String input, Object... param) {
+        return strip ? stripColor(format(input, param)) : format(input, param);
+    }
+
+    /**
+     * Returns a colorized string, this method normalizes the string by replacing all instances of
+     * the '§' char with a '&' char.
+     *
+     * @param input Provided input
+     * @return A color formatted string
+     */
+    public static @NotNull String translateColor(@Nullable String input) {
+        input = StringUtil.nonNull(input).replace(ChatColor.COLOR_CHAR, '&');
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
+
+    /**
+     * Returns a string that has all color codes removed from its content.
+     *
+     * @param input Provided input
+     * @return A colorless string
+     */
+    public static String stripColor(String input) {
+        return ChatColor.stripColor(input);
+    }
+
+    /*
+    ENSURING METHODS
+     */
 
     /**
      * Used to return a non-null string, this method accomplishes this by checking if the original input
