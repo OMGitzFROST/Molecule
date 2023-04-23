ca0415026b34dcf202a1e6af266e44ea90f7048a
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 16:38:43 -0700

    Alphabetized constants and added EXISTS constant

--- src/main/java/com/moleculepowered/api/updater/UpdateResult.java
+++ src/main/java/com/moleculepowered/api/updater/UpdateResult.java
@@ -3,22 +3,27 @@ package com.moleculepowered.api.updater;
 public enum UpdateResult {
 
     /**
-     * Indicates that the updater has determined that a newer version of this
-     * plugin is available for download
+     * Indicates that the updater has been disabled by the server owner and will prevent
+     * the updater from checking for new updates
      */
-    UPDATE_AVAILABLE,
+    DISABLED,
+    /**
+     * Indicates that an update is available, but it has already been downloaded
+     */
+    EXISTS,
     /**
      * Indicates that there are no available updates available for this plugin, please note
      * that if beta versions are disabled, the updater would still mark the result to this.
      */
     LATEST,
     /**
-     * Indicates that the updater has been disabled by the server owner and will prevent
-     * the updater from checking for new updates
-     */
-    DISABLED,
-    /**
      * Indicates that the result for the update check was unknown
      */
-    UNKNOWN
+    UNKNOWN,
+    /**
+     * Indicates that the updater has determined that a newer version of this
+     * plugin, but the updater is unable to download it using the download link
+     * provided.
+     */
+    UPDATE_AVAILABLE
 }
