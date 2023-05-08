72e74784a31bdf136464c1d9617e1fd29ae80993
OMGitzFROST <deltoro.steve98@gmail.com>
2023-05-07 23:56:38 -0700

    Moved #saveResource() to the FileUtil class

--- src/main/java/com/moleculepowered/api/MoleculeAPI.java
+++ src/main/java/com/moleculepowered/api/MoleculeAPI.java
@@ -1,11 +1,7 @@
 package com.moleculepowered.api;
 
 import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.io.InputStream;
-import java.util.Objects;
 
 public final class MoleculeAPI {
 
@@ -29,16 +25,5 @@ public final class MoleculeAPI {
      */
     public static void setPlugin(Plugin plugin) {
         MoleculeAPI.plugin = plugin;
-    }
-
-    /**
-     * Returns a resource from within MoleculeAPI, please note that this should not be used to
-     * return a resource from within your own plugin's resource folder.
-     *
-     * @param name Resource name
-     * @return A resource from MoleculeAPI
-     */
-    public static @NotNull InputStream getResource(@NotNull String name) {
-        return Objects.requireNonNull(MoleculeAPI.class.getClassLoader().getResourceAsStream(name));
     }
 }
