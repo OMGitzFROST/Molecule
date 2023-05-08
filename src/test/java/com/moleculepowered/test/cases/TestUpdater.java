29e0bcecc2e35630b1ad0b38c4b29719d4511503
OMGitzFROST <deltoro.steve98@gmail.com>
2023-05-07 23:47:38 -0700

    Added a test case that ensures updater works on the main thread as well as async

--- src/test/java/com/moleculepowered/test/cases/TestUpdater.java
+++ src/test/java/com/moleculepowered/test/cases/TestUpdater.java
@@ -1,5 +1,6 @@
 package com.moleculepowered.test.cases;
 
+import com.moleculepowered.api.event.updater.UpdateCompleteEvent;
 import com.moleculepowered.api.updater.Updater;
 import com.moleculepowered.api.updater.provider.BukkitProvider;
 import com.moleculepowered.api.updater.provider.GithubProvider;
@@ -8,22 +9,29 @@ import com.moleculepowered.api.updater.provider.SpigotProvider;
 import com.moleculepowered.api.util.Time;
 import com.moleculepowered.test.AbstractTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public final class TestUpdater extends AbstractTest {
 
-    @Test
-    @DisplayName("Ensure updater is working as intended")
-    public void testUpdater() {
+    private static Updater updater;
 
+    @BeforeAll
+    @DisplayName("Initialize the main updater object before the main tests")
+    public static void initializeUpdater() {
         // INITIALIZE UPDATER
-        Updater updater = new Updater(plugin)
+        updater = new Updater(plugin)
                 .addProvider(new GithubProvider("EssentialsX/Essentials"))
                 .addProvider(new SpigetProvider(9089))
                 .addProvider(new SpigotProvider(9089))
                 .addProvider(new BukkitProvider(93271));
         updater.initialize();
+    }
+
+    @Test
+    @DisplayName("Ensure updater is working as intended")
+    public void testUpdater() {
 
         // ASSERT INTERVAL WAS PARSED CORRECTLY AND CORRECTLY MIRRORS DEFAULT
         Assertions.assertEquals(updater.getInterval(), Time.parseInterval("3h"));
@@ -37,5 +45,15 @@ public final class TestUpdater extends AbstractTest {
 
         // ASSERT UPDATER IS ENABLED BY DEFAULT
         Assertions.assertTrue(updater.isEnabled());
+
+        // ENSURE UPDATE COMPLETE EVENT IS FIRED
+        server.getPluginManager().assertEventFired(UpdateCompleteEvent.class);
+    }
+
+    @Test
+    @DisplayName("Ensure updater runs well on the main thread and and async thread")
+    public void testScheduler() {
+        server.getScheduler().runTaskTimer(plugin, () -> updater.initialize(), 0, Time.parseInterval("5h"));
+        server.getScheduler().runTaskTimerAsynchronously(plugin, () -> updater.initialize(true), 0, Time.parseInterval("5h"));
     }
 }
