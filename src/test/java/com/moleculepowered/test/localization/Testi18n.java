c911b67b2bb17f60544b9fc36df547cf9c8170ce
OMGitzFROST <deltoro.steve98@gmail.com>
2023-05-08 17:37:38 -0700

    Shortened tested translations

--- src/test/java/com/moleculepowered/test/localization/Testi18n.java
+++ src/test/java/com/moleculepowered/test/localization/Testi18n.java
@@ -4,7 +4,6 @@ import com.moleculepowered.api.localization.i18n;
 import com.moleculepowered.test.AbstractTest;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -13,24 +12,15 @@ import static com.moleculepowered.api.localization.i18n.tl;
 
 public class Testi18n extends AbstractTest {
 
-    private static Testi18n instance;
-
     public Testi18n() {
-        instance = this;
-        i18n i18n = new i18n();
         i18n.setTranslator(this::translate);
-    }
-
-    @BeforeAll
-    public static void initialize() {
-        instance = new Testi18n();
     }
 
     @Test
     public void testLocalization() {
-        Assertions.assertEquals(tl(TestMessages.TEST_ONE.getPath()), "Hello world this is test #1");
-        Assertions.assertEquals(tl(TestMessages.TEST_TWO.getPath()), "Hello world this is test #2");
-        Assertions.assertEquals(tl(TestMessages.TEST_THREE.getPath()), "Hello world this is test #3");
+        Assertions.assertEquals(tl(TestMessages.TEST_ONE.getPath()), "Test #1");
+        Assertions.assertEquals(tl(TestMessages.TEST_TWO.getPath()), "Test #2");
+        Assertions.assertEquals(tl(TestMessages.TEST_THREE.getPath()), "Test #3");
     }
 
     private String translate(@NotNull String path) {
@@ -43,9 +33,9 @@ public class Testi18n extends AbstractTest {
 
     private enum TestMessages {
 
-        TEST_ONE("test.message.one", "Hello world this is test #1"),
-        TEST_TWO("test.message.two", "Hello world this is test #2"),
-        TEST_THREE("test.message.three", "Hello world this is test #3");
+        TEST_ONE("test.message.one", "Test #1"),
+        TEST_TWO("test.message.two", "Test #2"),
+        TEST_THREE("test.message.three", "Test #3");
 
         private final String message, path;
 
